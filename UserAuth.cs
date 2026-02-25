3e4f542e06e072183753fdc225bba1ea0e6f91ea
akhilmadhumenon <88308786+akhilmadhumenon@users.noreply.github.com>
2026-02-25 21:17:50 +0530

    AI: DevOS lifecycle update

--- UserAuth.cs
+++ UserAuth.cs
@@ -2,13 +2,6 @@ public class UserAuth
 {
     public bool CheckUser(string user)
     {
-        if (user != null)
-        {
-            if (user.Length > 0)
-            {
-                return true;
-            }
-        }
-        return false;
+        return !string.IsNullOrWhiteSpace(user);
     }
 }
